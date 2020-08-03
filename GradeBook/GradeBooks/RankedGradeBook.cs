79afb42ff74a26b2b575b28009baa3dfe486c182
Raúl Vega <jvega@bside.com.mx>
2020-08-03 15:31:41 -0500

    Module 3 Completed

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
@@ -6,6 +9,36 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            List<double> sortedGrades = Students.OrderByDescending(s => s.AverageGrade).Select(o => o.AverageGrade).ToList();
+            var averagePerc = Math.Round((double)20 / 100 * Students.Count);
+
+            if (averageGrade <= sortedGrades[0] && averageGrade > sortedGrades[(int)averagePerc - 1])
+            {
+                return 'A';
+            }
+            else if (averageGrade <= sortedGrades[(int)averagePerc] && averageGrade >= sortedGrades[(int)averagePerc * 2 - 1])
+            {
+                return 'B';
+            }
+            else if (averageGrade <= sortedGrades[(int)averagePerc * 2] && averageGrade >= sortedGrades[(int)averagePerc * 3 - 1])
+            {
+                return 'C';
+            }
+            else if (averageGrade <= sortedGrades[(int)averagePerc * 3] && averageGrade >= sortedGrades[(int)averagePerc * 4 - 1])
+            {
+                return 'D';
+            }
+
+            return 'F';
         }
     }
 }
