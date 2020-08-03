871cf6a4779df159f4dbfefd37d2dbed0763e4bc
Raúl Vega <jvega@bside.com.mx>
2020-08-03 15:53:12 -0500

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -43,7 +43,7 @@ namespace GradeBook.GradeBooks
 
         public override void CalculateStatistics()
         {
-            if (Students.Count < 5)
+            if (Students.Count < 5 && Students.Exists(s => s.Grades.Count == 0))
             {
                 throw new InvalidOperationException("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
             }
@@ -53,7 +53,7 @@ namespace GradeBook.GradeBooks
 
         public override void CalculateStudentStatistics(string name)
         {
-            if (Students.Count < 5)
+            if (Students.Count < 5 && Students.Exists(s => s.Grades.Count == 0))
             {
                 throw new InvalidOperationException("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
             }
